60c028b0d3fac0e1a500800bdc0160cd24f227ca
hadxr <hadikhan05@hotmail.co.uk>
2025-06-12 05:58:18 +0100

    Getting vercel working

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://your-frontend.vercel.app"])
 
 game = GameWrapper()
 
