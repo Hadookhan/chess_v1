aad3b1c5245d12cc9ac0eda3fc4710c3db524a98
hadxr <hadikhan05@hotmail.co.uk>
2025-06-12 05:40:14 +0100

    main

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 echo "Starting Backend..."
 source venv/bin/activate
+cd backend
 pip install -r "requirements.txt"
 python3 main.py &
-
+cd ..
 echo "Starting Frontend..."
 cd frontend
 npm run dev
