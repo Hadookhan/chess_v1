c1d558313340ee5bb65902627a474966351b4235
hadxr <hadikhan05@hotmail.co.uk>
2025-06-22 18:54:02 +0100

    Convert chess pieces

---
+++ frontend/convertLetterstoPieces.js
@@ -0,0 +1,11 @@
+import * as black from './assets/blackPieces';
+import * as white from './assets/whitePieces';
+
+const getPieceImage = (cell) => {
+  if (cell === ".") return null;
+  const piece = cell.toLowerCase();
+
+  return cell === cell.toUpperCase()
+    ? white[piece] // white piece
+    : black[piece]; // black piece
+};
